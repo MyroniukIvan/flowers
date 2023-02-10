3eb05a9d69492bddf2cba1041896da04bb41cf0a
ivanmironuk <vanyamironyuk5@gmail.com>
2023-02-10 15:13:00 +0200

    add: table.tsx

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import {DataGrid, GridColDef} from '@mui/x-data-grid';
 import styles from './table.module.scss'
+import {deleteDoc, doc} from "firebase/firestore";
+import {db} from "../../firebase/config";
+import {toast} from "react-toastify";
 
 const columns: GridColDef[] = [
     {field: 'imageURL', headerName: 'imageURL', width: 200, sortable: false, filterable: false, renderCell: (el) => <img src={el.value} alt={''} className={styles.img} />},
@@ -9,8 +12,24 @@ const columns: GridColDef[] = [
     {field: 'price', headerName: 'Price', type: 'number', width: 100, editable: true},
     {field: 'discount', headerName: 'discount', type: 'number', width: 100, editable: true},
     {field: 'type', headerName: 'type', type: 'string', width: 100, editable: true},
+    {field: 'delete', headerName: 'delete', editable: false, sortable: false, filterable: false, width: 50, renderCell: () => {
+        return (
+            <button onClick={deleteItem}>
+                X
+            </button>
+        )
+        }}
 ];
 
+export async function deleteItem() {
+    try {
+        await deleteDoc(doc(db, "flowers", 'name'));
+        toast.success('Successfully deleted')
+    }
+    catch (e) {
+        toast.error('error')
+    }
+}
 
 export default function DataTable({rows}: { rows: any }) {
     return (
--- pages/admin/viewProducts.tsx
+++ pages/admin/viewProducts.tsx
@@ -3,17 +3,26 @@ import {STORE_FLOWERS} from "../../redux/slice/productSlice";
 import {useDispatch, useSelector} from "react-redux";
 import React, {useEffect, useState} from "react";
 
-import DataTable from "./components/table/table";
+import DataTable from "../../components/table/table";
+import {doc, deleteDoc} from "firebase/firestore";
+import {db} from "../../firebase/config";
+import {toast} from "react-toastify";
+
 
 const ViewProducts = () => {
     const {data} = useFetch('flowers', 'name')
     const dispatch = useDispatch()
+
+
+
 
     useEffect(() => {
         dispatch(STORE_FLOWERS({
             products: data
         }))
     }, [dispatch, data])
+
+
     console.log(data)
     return (
         <div>
