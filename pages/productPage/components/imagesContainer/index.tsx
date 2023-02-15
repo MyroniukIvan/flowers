8e5f11e313221d8ad0e49bbb02be8ebc4247f7d6
Evgeniy <zhrkacomua@gmail.com>
2023-02-15 20:23:11 +0200

    add:  slider

--- pages/productPage/components/imagesContainer/index.tsx
+++ pages/productPage/components/imagesContainer/index.tsx
@@ -4,29 +4,40 @@ import DiscountImage from "./components/DiscountImage";
 import ImageBanner from "./components/ImageBanner";
 import redRose from '../../../../assets/redRose.png'
 import gift from '../../../../assets/gift.png'
+import candy from '../../../../assets/candy.png'
+import box from '../../../../assets/box.png'
+import Slider from '../sliderBanner/slider'
+
+
 
 const Index = () => {
 
     return (
-        <div className={styles.image__wrapper}>
-            <div className={styles.element1}>
-                <DiscountImage/>
-            </div>
-            <div className={styles.element2}>
-                <ImageBanner image={redRose} title={''}/>
-            </div>
-            <div className={styles.element3}>
-                <ImageBanner image={gift} title={''}/>
-            </div>
-            <div className={styles.element4}>
-                <ImageBanner image={redRose} title={''}/>
-            </div>
-            <div className={styles.element5}>
-                <ImageBanner image={redRose} title={''}/>
+        <div className={styles.images__container}>
+            <div className={styles.image__wrapper}>
+                <div className={styles.element1}>
+                    <DiscountImage/>
+                </div>
+                <div className={styles.element2}>
+                    <ImageBanner image={redRose} title={'Рози'}/>
+                </div>
+                <div className={styles.element3}>
+                    <ImageBanner image={gift} title={'Подарунки'}/>
+                </div>
+                <div className={styles.element4}>
+                    <ImageBanner image={candy} title={'Подарункові кошики'}/>
+                </div>
+                <div className={styles.element5}>
+                    <ImageBanner image={box} title={'Квіти в коробці'}/>
+                </div>
             </div>
 
-
+            <div className={styles.slider__wrapper}>
+<Slider/>
+            </div>
         </div>
+
+
 
     );
 };
